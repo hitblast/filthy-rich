c89aae68cae5d73e9d0d1eeb09bb7eb3752ee1a1
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-04-23 19:49:31 +0600

    add show_errors impl for example

--- examples/indefinite.rs
+++ examples/indefinite.rs
@@ -7,7 +7,9 @@ use filthy_rich::{
 #[tokio::main]
 async fn main() -> Result<()> {
     let mut runner = PresenceRunner::new("1463450870480900160")
-        .on_ready(|data| println!("Connected to user: {}", data.user.username));
+        .on_ready(|data| println!("Connected to user: {}", data.user.username))
+        .show_errors() // enables verbose error logging
+    ;
 
     let client = runner.run(true).await?;
 
