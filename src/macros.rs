cf0a32210b59fd2856b0d34312c9fe3ca7bb8457
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-05-07 21:14:07 +0600

    chore: document macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,3 +1,8 @@
+//! Public, crate-only macros
+//!
+
+/// Generates a function `$name` which takes a `$param` and sets the
+/// `$name` field of a struct to be a `None`-filtered `String`.
 #[macro_export]
 macro_rules! nf {
     ($name:ident, $doc:expr, $param:ident) => {
@@ -11,6 +16,8 @@ macro_rules! nf {
     };
 }
 
+/// Generates a function `$name` which returns an `Option<&str>` by
+/// dereferencing the `$name` field of a struct.
 #[macro_export]
 macro_rules! ds {
     ($name:ident, $doc:expr) => {
